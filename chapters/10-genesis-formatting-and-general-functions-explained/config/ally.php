5d0150e9b7c57665091c77beebb7aba88e4c7377
Nick Croft <nick@copyblogger.com>
2019-03-16 08:41:45 -0400

    add example config file

---
+++ chapters/10-genesis-formatting-and-general-functions-explained/config/ally.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Return the accessibility config for `genesis-accessibility` theme support.
+ *
+ * @package genesis-explained
+ */
+
+return array(
+	'404-page',
+	'drop-down-menu',
+	'headings',
+	'rems',
+	'search-form',
+	'skip-links',
+);
